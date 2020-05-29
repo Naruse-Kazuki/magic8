07d8f38f7d41c5d41662586366c964adf9573f73
Naruse-Kazuki <kuroraido@icloud.com>
2020-05-29 23:02:15 +0900

    comp

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,43 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() {
+  return runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          title: Text('Ask Me Anything'),
+          backgroundColor: Colors.blue,
+        ),
+        body: MagicPage(),
+      ),
+    ),
+  );
+}
+
+class MagicPage extends StatefulWidget {
+  @override
+  _MagicPageState createState() => _MagicPageState();
+}
+
+class _MagicPageState extends State<MagicPage> {
+  int magic8 = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Expanded(
+        child: FlatButton(
+          onPressed: () {
+            setState(() {
+              magic8 = Random().nextInt(5) + 1;
+            });
+          },
+          child: Image.asset('images/ball$magic8.png'),
+        ),
       ),
     );
+  }
+}
